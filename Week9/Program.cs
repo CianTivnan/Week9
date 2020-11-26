11b8847067e7270201f5c733d238255636d34f0e
CianTivnan <cian.tivnan@yahoo.ie>
2020-11-26 11:47:45 +0000

    Tasks 1-9 complete

--- Week9/Program.cs
+++ Week9/Program.cs
@@ -10,6 +10,32 @@ namespace Week9
     {
         static void Main(string[] args)
         {
+            Song song1 = new Song("Breaking the Law", "Judas Priest", 2.33, Genre.Rock);
+            Song song2 = new Song("The Coffin Ships", "Primordial", 9.58, Genre.Rock);
+            Song song3 = new Song("DEJA VU", "Dave Rodgers", 4.20, Genre.Dance);
+            Song song4 = new Song("Chemical Love", "KEVIN&CHERRY", 5.45, Genre.Dance);
+            Song song5 = new Song("Lightning & Snow", "Woods Of Ypres", 4.40, Genre.Rock);
+
+            List<Song> PlayList = new List<Song>();
+            PlayList.Add(song1);
+            PlayList.Add(song2);
+            PlayList.Add(song3);
+            PlayList.Add(song4);
+            PlayList.Add(song5);
+
+            DisplayPlaylist(PlayList);
+
+
+        }
+
+        static void DisplayPlaylist(List<Song> Playlist)
+        {
+            Console.WriteLine("{0,-20}{1,-20}{2,-10}{3,-10}" ,"Artist" ,"Song" ,"Duration" ,"Genre");
+
+            foreach (Song song in Playlist)
+            {
+                Console.WriteLine(song.ToString());
+            }
         }
     }
 }
---
+++ Week9/Song.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9
+{
+    class Song
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public double Duration { get; set; }
+
+        public Genre Genre { get; set; }
+
+        //still not entirely sure how to imlement the linked constructors
+
+        public Song()
+        {
+            Title = "Unknown";
+            Artist = "Unknown";
+            Duration = 0.0;
+            Genre = 0;
+        }
+
+        public Song(string title, string artist, double duration, Genre genre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-20}{1,-20}{2,-10}{3,-10}" ,Title ,Artist ,Duration ,Genre);
+        }
+    }
+
+    enum Genre
+    {
+        Rock = 0,
+        Pop = 1,
+        Dance = 2,
+        Other =3
+    }
+
+
+}
